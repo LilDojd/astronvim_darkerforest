cffdf0e726043325ac9ce04b7f024a726eea323e
LilDojd <yawner@protonmail.com>
2024-02-20 19:49:35 +0400

    Switch back to astrocommunity pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,6 @@ return {
 
     { import = "astrocommunity.colorscheme.catppuccin" },
     { import = "astrocommunity.completion.copilot-lua-cmp" },
-    { import = "astrocommunity.scrolling.cinnamon-nvim" }
+    { import = "astrocommunity.scrolling.cinnamon-nvim" },
+    { import = "astrocommunity.pack.rust" },
 }
